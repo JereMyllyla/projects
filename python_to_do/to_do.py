5d275479b17a99ab9e940e774661aad9f1c6d5c8
Jere <ad0917.student.jamk.fi>
2023-12-15 13:35:49 +0200

    Can now remove single tasks and all tasks from file. Can also mark task as done or undone.

--- python_to_do/to_do.py
+++ python_to_do/to_do.py
@@ -5,9 +5,12 @@
 #   Add a task to the to-do list.
 #   Display the current tasks in the to-do list.
 #   Mark tasks as completed.
+#   Mark tasks as uncompleted.
 #   Remove tasks from the to-do list.
+#   Empty the task list
 
 import json
+import time
 
 def load_tasks_from_file(file_path):
     try:
@@ -34,38 +37,77 @@ def add_task_to_list():
     tasks_to_do.append(json_to_add)
     save_tasks_to_file(file_path, tasks_to_do)
     
-def mark_task_done():
-    to_mark_done = input("What did you do?")
-    to_mark_done = to_mark_done[:-2]
+def mark_task_done(task_id):
+    tasks_to_do, _ = load_tasks_from_file(file_path)
+    for task in tasks_to_do:
+        if task["id"] == task_id:
+            if task["completed"] == False:
+                task["completed"] = True
+                break
+            else:
+                task["completed"] = False
+                break
+    save_tasks_to_file(file_path, tasks_to_do)
 
-def remove_task_from_list():
-    to_remove = input("What to remove")
-    tasks_to_do.remove(to_remove)
-
+def remove_task_from_list(task_id):
+    tasks_to_do, _ = load_tasks_from_file(file_path)
+    tasks_to_do = [task for task in tasks_to_do if task["id"] != task_id]
+    save_tasks_to_file(file_path, tasks_to_do)
+    
 def view_tasks_in_list():
     tasks_to_do, max_id = load_tasks_from_file(file_path)
     for task in tasks_to_do:
-        print(task)
+        task_id = (task["id"])
+        task_name = (task["task"])
+        if (task["completed"]) == False:
+            task_string = f"{task_id}: {task_name} [ ]"
+            print(task_string)
+        else:
+            task_string = f"{task_id}: {task_name} [x]"
+            print(task_string)
+
+def delete_all_tasks():
+    tasks_to_do, _ = load_tasks_from_file(file_path)
+    tasks_to_do = []
+    save_tasks_to_file(file_path, tasks_to_do)  
 
 file_path = 'tasks.json'
-
 tasks_to_do, max_id = load_tasks_from_file(file_path)
 
-user_input = input("What to do? add, delete, view or exit: ")
+print("To-Do:")
+view_tasks_in_list()
+user_input = input("What to do? add, done, remove or exit: ")
 
 while user_input != "exit":
     if user_input == "add":
         add_task_to_list()
     elif user_input == "done":
-        mark_task_done()
-    elif user_input == "delete":
-        remove_task_from_list()
-    elif user_input == "view":
-        view_tasks_in_list()
+        try:
+            id_to_done = int(input("What number did you do: "))
+            mark_task_done(id_to_done)
+        except:
+            print("Please use integers only.")
+    elif user_input == "remove":
+        try:
+            id_to_remove = int(input("What number to remove: "))
+            remove_task_from_list(id_to_remove)
+        except:
+            print("Please use integers only.")
+    elif user_input == "DELETE":
+        print("WARNING! You are about to delete the whole list.")
+        print("Re-type 'DELETE' to confirm, anything else to cancel.")
+        delete_confirmation = input()
+        if delete_confirmation == "DELETE":
+            delete_all_tasks()
+        else:
+            print("Cancelled.")
     else:
-        print("ERROR: use an accepted keyword!")     
-    user_input = input("What to do? add, delete or view: ")
+        print("ERROR: Use one of the accepted keywords. (add, done, remove or exit).")
+    print("To-Do:")
+    view_tasks_in_list()         
+    user_input = input("What to do? add, done, remove or exit: ")
 
 
-#save_tasks_to_file(file_path, tasks_to_do)
-print("Goodbye")   
+print("Goodbye!")   
+# A small delay before closing window.
+time.sleep(2)
